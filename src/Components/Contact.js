4ed983c5e9c30994437a0fb59c7378f7abcc2377
pa-suarezm <pabloasuarezm@hotmail.com>
2020-10-11 02:17:03 -0500

    Contact done

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+
 class Contact extends Component {
   render() {
 
@@ -20,47 +24,36 @@ class Contact extends Component {
          <div className="row section-head">
 
             <div className="two columns header-col">
-
-               <h1><span>Get In Touch.</span></h1>
-
+               <h1><span><FontAwesomeIcon icon = {faEnvelope}/> Contacto</span></h1>
             </div>
-
+            
             <div className="ten columns">
-
                   <p className="lead">{message}</p>
-
             </div>
-
          </div>
 
          <div className="row">
             <div className="eight columns">
-
-               <form action="" method="post" id="contactForm" name="contactForm">
+               <form action="mailto:pabloasuarezm@hotmail.com" method="post" encType="text/plain" id="contactForm" name="contactForm">
 					<fieldset>
 
                   <div>
-						   <label htmlFor="contactName">Name <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
+						   <label htmlFor="contactName">Nombre <span className="required">*</span></label>
+						   <input type="text" defaultValue="" size="35" id="contactName" name="name" onChange={this.handleChange}/>
                   </div>
 
                   <div>
-						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
+						   <label htmlFor="contactSubject">Asunto</label>
+						   <input type="text" defaultValue="" size="35" id="contactSubject" name="subject" onChange={this.handleChange}/>
                   </div>
 
                   <div>
-						   <label htmlFor="contactSubject">Subject</label>
-						   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
+                     <label htmlFor="contactMessage">Mensaje <span className="required">*</span></label>
+                     <textarea cols="50" rows="15" id="contactMessage" name="comment"></textarea>
                   </div>
 
                   <div>
-                     <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
-                  </div>
-
-                  <div>
-                     <button className="submit">Submit</button>
+                     <button className="submit">Enviar</button>
                      <span id="image-loader">
                         <img alt="" src="images/loader.gif" />
                      </span>
@@ -68,49 +61,28 @@ class Contact extends Component {
 					</fieldset>
 				   </form>
 
-           <div id="message-warning"> Error boy</div>
+           <div id="message-warning">Error boy</div>
 				   <div id="message-success">
-                  <i className="fa fa-check"></i>Your message was sent, thank you!<br />
+                  <i className="fa fa-check"></i>Se envió el mensaje. ¡Gracias!<br />
 				   </div>
            </div>
 
 
             <aside className="four columns footer-widgets">
                <div className="widget widget_contact">
-
-					   <h4>Address and Phone</h4>
+					   <h4>Dirección, correo, y teléfono</h4>
 					   <p className="address">
 						   {name}<br />
 						   {street} <br />
 						   {city} {state}, {zip}<br />
-						   <span>{phone}</span>
+						   <span>{phone}</span><br />
+                     {email}
 					   </p>
 				   </div>
-
-               <div className="widget widget_tweets">
-                  <h4 className="widget-title">Latest Tweets</h4>
-                  <ul id="twitter">
-                     <li>
-                        <span>
-                        This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                        Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
-                        <a href="#">http://t.co/CGIrdxIlI3</a>
-                        </span>
-                        <b><a href="#">2 Days Ago</a></b>
-                     </li>
-                     <li>
-                        <span>
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-                        eaque ipsa quae ab illo inventore veritatis et quasi
-                        <a href="#">http://t.co/CGIrdxIlI3</a>
-                        </span>
-                        <b><a href="#">3 Days Ago</a></b>
-                     </li>
-                  </ul>
-		         </div>
             </aside>
-      </div>
-   </section>
+         </div>
+
+      </section>
     );
   }
 }
