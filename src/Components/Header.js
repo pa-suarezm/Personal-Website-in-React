3c686efd0c7aee6fbd0360429a20367d1dd17dd1
pa-suarezm <pabloasuarezm@hotmail.com>
2020-10-19 22:04:01 -0500

    Header - More code cleanup

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -17,33 +17,31 @@ import { FormattedMessage } from 'react-intl';
 class Header extends Component {
   render() {
 
-    if(this.props.data){
-      var networks = 
-      [
-         <li key="linkedin"><a href="https://www.linkedin.com/in/pablo-andres-suarez/"><FontAwesomeIcon icon = {faLinkedinIn} /></a></li>,
-         <li key="github"><a href="https://github.com/pa-suarezm"><FontAwesomeIcon icon = {faGithub}/></a></li>,
-         <li key="itch.io"><a href="https://pa-suarezm.itch.io/"><FontAwesomeIcon icon = {faItchIo} /></a></li>,
-         <li key="facebok"><a href="https://www.facebook.com/pabloasuarezm/"><FontAwesomeIcon icon = {faFacebookF} /></a></li>,
-         <li key="twitter"><a href="https://twitter.com/PabloSuarezCSC"><FontAwesomeIcon icon = {faTwitter} /></a></li>,
-         <li key="instagram"><a href="https://www.instagram.com/suarezcsc/"><FontAwesomeIcon icon = {faInstagram} /></a></li>,
-         <li key="whatsapp"><a href="https://wa.me/573008801016"><FontAwesomeIcon icon = {faWhatsapp} /></a></li>,
-         <li key="outlook"><a href="mailto:pabloasuarezm@hotmail.com"><FontAwesomeIcon icon = {faWindows} /></a></li>,
-         <li key="gmail"><a href="mailto:pabloasuarezm@gmail.com"><FontAwesomeIcon icon = {faGoogle} /></a></li>
-      ];
-    }
+   var networks = 
+   [
+      <li key="linkedin"><a href="https://www.linkedin.com/in/pablo-andres-suarez/"><FontAwesomeIcon icon = {faLinkedinIn} /></a></li>,
+      <li key="github"><a href="https://github.com/pa-suarezm"><FontAwesomeIcon icon = {faGithub}/></a></li>,
+      <li key="itch.io"><a href="https://pa-suarezm.itch.io/"><FontAwesomeIcon icon = {faItchIo} /></a></li>,
+      <li key="facebok"><a href="https://www.facebook.com/pabloasuarezm/"><FontAwesomeIcon icon = {faFacebookF} /></a></li>,
+      <li key="twitter"><a href="https://twitter.com/PabloSuarezCSC"><FontAwesomeIcon icon = {faTwitter} /></a></li>,
+      <li key="instagram"><a href="https://www.instagram.com/suarezcsc/"><FontAwesomeIcon icon = {faInstagram} /></a></li>,
+      <li key="whatsapp"><a href="https://wa.me/573008801016"><FontAwesomeIcon icon = {faWhatsapp} /></a></li>,
+      <li key="outlook"><a href="mailto:pabloasuarezm@hotmail.com"><FontAwesomeIcon icon = {faWindows} /></a></li>,
+      <li key="gmail"><a href="mailto:pabloasuarezm@gmail.com"><FontAwesomeIcon icon = {faGoogle} /></a></li>
+   ];
 
-    return (
-      <header id="home">
+   return (
+   <header id="home">
 
       <nav id="nav-wrap">
 
          <a className="mobile-btn" href="#nav-wrap"><FormattedMessage id="ShowNav" /></a>
-	      <a className="mobile-btn" href="#home"><FormattedMessage id="HideNav" /></a>
+         <a className="mobile-btn" href="#home"><FormattedMessage id="HideNav" /></a>
 
          <ul id="nav" className="nav">
             <li className="current"><a className="smoothscroll" href="#home"><FormattedMessage id="Home" /></a></li>
             <li><a className="smoothscroll" href="#about"><FormattedMessage id="About" /></a></li>
-	         <li><a className="smoothscroll" href="#resume"><FormattedMessage id="Resume" /></a></li>
+            <li><a className="smoothscroll" href="#resume"><FormattedMessage id="Resume" /></a></li>
             <li><a className="smoothscroll" href="#portfolio"><FormattedMessage id="Projects" /></a></li>
             <li><a className="smoothscroll" href="#contact"><FormattedMessage id="Contact" /></a></li>
          </ul>
